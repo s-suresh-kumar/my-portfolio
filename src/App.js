4087fdb9ce5c7386b22218f74db6a14f40b3bd6b
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-10-01 08:23:21 -0400

    implement and integrate navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import About from "./pages/About";
 
 function App() {
   return (
     <Router>
       <div>
+        <Navbar />
         <Route exact path="/" component={About} />
         <Route exact path="/about" component={About} />
       </div>
