994f049d1fe87a30eaee2f25675a1a9a670f3d2a
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-10-02 23:17:14 -0400

    Updates

---
+++ precache-manifest.6d4d97ceb3c0b55a6f475156e3d4c248.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d8a805c3bc687af3d5b5f8567596da",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "0dc24d0130551a3f35f8",
+    "url": "/my-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "119ae5aab1e43501653a",
+    "url": "/my-portfolio/static/css/main.fb45a373.chunk.css"
+  },
+  {
+    "revision": "0dc24d0130551a3f35f8",
+    "url": "/my-portfolio/static/js/2.b3e8436d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.b3e8436d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119ae5aab1e43501653a",
+    "url": "/my-portfolio/static/js/main.efb503cd.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
