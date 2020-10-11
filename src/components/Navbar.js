485111116c9fb9d3144c0410f7266954b8ded166
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-10-11 18:06:13 -0400

    navbar style change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import "./navbar.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <Link className="navbar-brand" to="/">
         Suresh S Kumar
       </Link>
