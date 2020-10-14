7a3d1c3185ca4d8e2dbe051f91f2759dea2c7268
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-10-14 17:35:29 -0400

    Fix warnings in Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,32 +6,17 @@ function Project(props) {
   return (
     <div className="card">
       <div className="img-container">
-        <a target="_blank" href={props.deployLink}><img alt={props.name} src={process.env.PUBLIC_URL + props.image} width="400" height="274" /> </a>
+        <a target="_blank" rel="noopener noreferrer" href={props.deployLink}><img alt={props.name} src={process.env.PUBLIC_URL + props.image} width="400" height="274" /> </a>
 
       </div>
       <div className="content text-center">
-        <a target="_blank" className="myGButton" href={props.deployLink} role="button">{props.name}</a>
+        <a target="_blank" rel="noopener noreferrer" className="myGButton" href={props.deployLink} role="button">{props.name}</a>
 
-        <a target="_blank" className="myYButton" href={props.githubRepo} role="button">Code</a>
+        <a target="_blank" rel="noopener noreferrer" className="myYButton" href={props.githubRepo} role="button">Code</a>
 
       </div>
     </div >
   );
 }
-{/* <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Deployed:</strong> {props.deployLink}
-          </li>
-          <li>
-            <strong>Code:</strong> {props.githubRepo}
-          </li>
-        </ul>
-      </div>
-    </div> */}
-
 
 export default Project;
