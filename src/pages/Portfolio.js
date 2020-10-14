1f8e0a967278c8a50a1e265ab312d1de58c28b9a
Suresh SK <suresh.uncbc.coding@gmail.com>
2020-10-14 17:47:29 -0400

    Fix warnings in Portfolio.js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,8 +1,4 @@
 import React from "react";
-import Header from "../components/Header";
-import Container from "../components/Container";
-import Row from "../components/Row";
-import Col from "../components/Col";
 import Wrapper from "../components/Wrapper";
 import Title from "../components/Title";
 import Project from "../components/Project";
@@ -14,36 +10,27 @@ class Portfolio extends React.Component {
     projects
   };
 
-  // removeFriend = id => {
-  //   // Filter this.state.projects for projects with an id not equal to the id being removed
-  //   const projects = this.state.projects.filter(friend => friend.id !== id);
-  //   // Set this.state.projects equal to the new projects array
-  //   this.setState({ projects });
-  // };
 
-  // Map over this.state.projects and render a FriendCard component for each friend object
+  // Map over this.state.projects and render a Project component for each project object
   render() {
     return (
       <Wrapper>
         <Title>Projects</Title>
 
-        {/* <div className="row"> */}
-          {
-            this.state.projects.map(project => (
+        {
+          this.state.projects.map(project => (
 
-              <Project
-                // removeFriend={this.removeFriend}
-                id={project.id}
-                key={project.id}
-                name={project.name}
-                image={project.image}
-                githubRepo={project.githubRepo}
-                deployLink={project.deployLink}
-              />
+            <Project
+              id={project.id}
+              key={project.id}
+              name={project.name}
+              image={project.image}
+              githubRepo={project.githubRepo}
+              deployLink={project.deployLink}
+            />
 
-            ))
-          }
-        {/* </div> */}
+          ))
+        }
 
       </Wrapper >
     );
