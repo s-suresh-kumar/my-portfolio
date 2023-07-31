104ccb6235949ed5bf4ddc1c5bc874123e93984c
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2023-07-30 21:55:41 -0400

    change image size changed

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -27,24 +27,24 @@ function About() {
                 </li>
                 <li>
                   <strong className="tab4">Phone:
-                  919-760-3970</strong>
+                    919-760-3970</strong>
                 </li>
                 <li>
                   <strong>Github:
-                <a target="_blank" rel="noopener noreferrer" className="tab4" href="https://github.com/s-suresh-kumar"> s-suresh-kumar</a></strong>
+                    <a target="_blank" rel="noopener noreferrer" className="tab4" href="https://github.com/s-suresh-kumar"> s-suresh-kumar</a></strong>
                 </li>
                 <li>
                   <strong>LinkedIn:
-                <a target="_blank" rel="noopener noreferrer" className="tab4" href="https://www.linkedin.com/in/suresh-s-kumar"> suresh-s-kumar</a>
+                    <a target="_blank" rel="noopener noreferrer" className="tab4" href="https://www.linkedin.com/in/suresh-s-kumar"> suresh-s-kumar</a>
                   </strong>
                 </li>
                 <li>
                   <strong>Portfolio:
-                <a target="_blank" rel="noopener noreferrer" className="tab4" href="https://s-suresh-kumar.github.io/responsivePortfolio/portfolio.html">  my-portfolio</a></strong>
+                    <a target="_blank" rel="noopener noreferrer" className="tab4" href="https://s-suresh-kumar.github.io/responsivePortfolio/portfolio.html">  my-portfolio</a></strong>
                 </li>
                 <li className="mb-2 pb-2">
                   <strong>Resume:
-                <a target="_blank" rel="noopener noreferrer" className="tab4" href={process.env.PUBLIC_URL + "/assets/Resume-Suresh-Kumar-09-28-2020.pdf"}>  My resume</a></strong>
+                    <a target="_blank" rel="noopener noreferrer" className="tab4" href={process.env.PUBLIC_URL + "/assets/Resume-Suresh-Kumar-09-28-2020.pdf"}>  My resume</a></strong>
                 </li>
               </ul>
             </div>
@@ -56,44 +56,45 @@ function About() {
           <Col size="md-12">
             <h1>About Me</h1>
           </Col>
-        </Row>
-        <Row>
-          <Col size="md-12">
-            <img src={process.env.PUBLIC_URL + authorPic} className=" float-left myPic" alt="Suresh Kumar" />
-            <p>
-              I live in Cary, North Carolina. I am a natural problem solver. I
-              like to add new features in existing products and software,
-              improve functionality and add value. I like to debug and provide
-              fixes to customer or user found issues to make the software /
-              product robust. I have extensively worked on different telecom
-              products in the area of application software and system software
-              using languages such as C , C++. I have varied experience in
-              management plane, control plane of different core and edge
-              telecome products such as Digital Switch (Telephone), ISDN
-              customer premises equipment, Mobile Switching Center (S-12),
-              Signaling Transfer Point product, Fiber To The User APON / BPON/
-              GPON telecom access products. I have extensive Real Time Operating
-              System based system application software development in a
-              distributed highly available fault tolerant systems. I intend to
-              keep myself abreast with ever changing emerging software
-              technologies. Currently I am interested in Full Stack Web
-              development and my dream is to master Full Stack Web Development
-              technologies and become a Full Stack Web developer.
-            </p>
-            <p>
-              I am detail oriented collaborative software engineer I am adept in
-              both agile and SDLC sotware development methologies. I have
-              extensive experience in all phases of software development right
-              from Requirements gathering to Integration Testing and supporting
-              Test Team, Customer Focus Teams. I have worked and solved several
-              customer reported complex issues in a timely manner and supported
-              customer facing teams. I want to bring my software development and
-              debugging skills to Full Stack Development in both front-end and
-              back-end areas.
-            </p>
+          <Row>
+            <Col size="md-12">
+              <img height="150px" width="150px" src={process.env.PUBLIC_URL + authorPic} className=" float-left myPic" alt="Suresh Kumar" />
+              <p>
+                I live in Cary, North Carolina. I am a natural problem solver. I
+                like to add new features in existing products and software,
+                improve functionality and add value. I like to debug and provide
+                fixes to customer or user found issues to make the software /
+                product robust. I have extensively worked on different telecom
+                products in the area of application software and system software
+                using languages such as C , C++. I have varied experience in
+                management plane, control plane of different core and edge
+                telecome products such as Digital Switch (Telephone), ISDN
+                customer premises equipment, Mobile Switching Center (S-12),
+                Signaling Transfer Point product, Fiber To The User APON / BPON/
+                GPON telecom access products. I have extensive Real Time Operating
+                System based system application software development in a
+                distributed highly available fault tolerant systems. I intend to
+                keep myself abreast with ever changing emerging software
+                technologies. Currently I am interested in Full Stack Web
+                development and my dream is to master Full Stack Web Development
+                technologies and become a Full Stack Web developer.
+              </p>
+              <p>
+                I am detail oriented collaborative software engineer I am adept in
+                both agile and SDLC sotware development methologies. I have
+                extensive experience in all phases of software development right
+                from Requirements gathering to Integration Testing and supporting
+                Test Team, Customer Focus Teams. I have worked and solved several
+                customer reported complex issues in a timely manner and supported
+                customer facing teams. I want to bring my software development and
+                debugging skills to Full Stack Development in both front-end and
+                back-end areas.
+              </p>
 
-          </Col>
-        </Row>
+            </Col>
+          </Row>
+
+          </Row>
       </Container>
     </div >
   );
